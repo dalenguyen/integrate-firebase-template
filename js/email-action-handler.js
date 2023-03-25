e8645900789c8e3f554da959e37b61cb89c7fa8b
dalenguyen <dale@dalenguyen.me>
2023-03-25 23:37:02 +0700

    add handleVerifyEmail sample

--- js/email-action-handler.js
+++ js/email-action-handler.js
@@ -5,7 +5,7 @@ const getURLQueryParam = (name) => {
   return urlParams.get(name)
 }
 
-function handleResetPassword(auth, actionCode, continueUrl, lang) {
+const handleResetPassword = (auth, actionCode, continueUrl, lang) => {
   // Localize the UI to the selected language as determined by the lang
   // parameter.
 
@@ -51,6 +51,30 @@ function handleResetPassword(auth, actionCode, continueUrl, lang) {
     })
 }
 
+const handleVerifyEmail = (auth, actionCode, continueUrl, lang) => {
+  // Localize the UI to the selected language as determined by the lang
+  // parameter.
+  // Try to apply the email verification code.
+  auth
+    .applyActionCode(actionCode)
+    .then((resp) => {
+      console.log({ resp })
+      // Email address has been verified.
+
+      // TODO: Display a confirmation message to the user.
+      // You could also provide the user with a link back to the app.
+
+      // TODO: If a continue URL is available, display a button which on
+      // click redirects the user back to the app via continueUrl with
+      // additional state determined from that URL's parameters.
+    })
+    .catch((error) => {
+      console.error(error)
+      // Code is invalid or expired. Ask the user to verify their email address
+      // again.
+    })
+}
+
 ;(function ($) {
   'use strict'
 
@@ -83,10 +107,10 @@ function handleResetPassword(auth, actionCode, continueUrl, lang) {
           //     // Display email recovery handler and UI.
           //     handleRecoverEmail(auth, actionCode, lang);
           //     break;
-          //   case 'verifyEmail':
-          //     // Display email verification handler and UI.
-          //     handleVerifyEmail(auth, actionCode, continueUrl, lang);
-          //     break;
+          case 'verifyEmail':
+            // Display email verification handler and UI.
+            handleVerifyEmail(auth, actionCode, continueUrl, lang)
+            break
           default:
           // Error: invalid mode.
         }
